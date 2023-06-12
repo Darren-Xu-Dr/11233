863f9820a405ee23ca9b97a7ebb0896dfe77743e
Darren-Xu-Dr <a0932135922@gmail.com>
2023-06-12 23:31:27 +0800

    Update for First commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const cards = document.querySelectorAll('.memory-card');
 const timerElement = document.getElementById('timer');
+const messageElement = document.getElementById('message');
+const resetButton = document.getElementById('reset-button');
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -9,7 +11,7 @@ let seconds = 0;
 
 function flipCard() {
   if (lockBoard || this === firstCard) return;
-  
+
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
@@ -53,7 +55,13 @@ function startTimer() {
   if (!timerInterval) {
     timerInterval = setInterval(() => {
       seconds++;
-      timerElement.textContent = seconds;
+      timerElement.textContent = formatTime(seconds);
+
+      if (seconds >= 60) {
+        stopTimer();
+        showMessage("You failed! Try again.");
+        resetButton.style.display = 'block';
+      }
     }, 1000);
   }
 }
@@ -67,15 +75,48 @@ function checkWin() {
   const matchedCards = document.querySelectorAll('.memory-card.flip');
   if (matchedCards.length === cards.length) {
     stopTimer();
-    // Show a message or perform any other action to indicate that the player has won
+    showMessage("Congratulations! You won!");
+    resetButton.style.display = 'block';
   }
 }
 
-(function shuffle() {
+function formatTime(seconds) {
+  const remainingSeconds = 60 - seconds;
+  return padZero(remainingSeconds);
+}
+
+function padZero(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
+function showMessage(text) {
+  messageElement.textContent = text;
+  messageElement.style.display = 'block';
+}
+
+function resetGame() {
+  stopTimer();
+  resetButton.style.display = 'none';
+  messageElement.style.display = 'none';
+  seconds = 0;
+  timerElement.textContent = formatTime(seconds);
+  cards.forEach(card => {
+    card.classList.remove('flip');
+    card.addEventListener('click', flipCard);
+  });
+  shuffleCards();
+}
+
+function shuffleCards() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
+}
+
+(function initializeGame() {
+  resetButton.addEventListener('click', resetGame);
+  shuffleCards();
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
